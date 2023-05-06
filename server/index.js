df0726631175c00f46994fc6d12d592f9c3e850f
ysilkov <ysilkov@ukr.net>
2023-05-06 12:09:30 +0300

    update

--- server/index.js
+++ server/index.js
@@ -2,22 +2,23 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./router.js";
 import cors from "cors";
+import dotenv from 'dotenv';
+dotenv.config();
 
-const PORT = 4000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-const DBL_URL = `mongodb+srv://user:user@cluster0.ju1rfue.mongodb.net/?retryWrites=true&w=majority`;
+
 
 app.use("/api", router);
 
 async function startApp() {
   try {
-    await mongoose.connect(DBL_URL, {
+    await mongoose.connect(process.env.DBL_URL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
-    app.listen(PORT, () => console.log("SERVER STARTED ON PORT 4000"));
+    app.listen(process.env.PORT, () => console.log("SERVER STARTED ON PORT 4000"));
   } catch (e) {
     console.log(e);
   }
